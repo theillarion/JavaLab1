61cfebeef0cc3c8668373d5430269d0ac814c173
Illarion <pro100illarion@gmail.com>
2022-10-17 15:15:07 +0300

    Added method clone

--- src/IShape.java
+++ src/IShape.java
@@ -1,6 +1,7 @@
-public interface IShape extends IMoveable
+public interface IShape extends IMoveable, Cloneable
 {
-	public double square();
-	public double length();
-	public boolean cross(IShape i);
+	double square();
+	double length();
+	boolean cross(IShape i);
+	IShape clone();
 }
